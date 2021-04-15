c3581dfe788fe7e32a0dfb9068f69aa715029629
Bruce0615 <bruce20140615@gmail.com>
2021-04-15 12:14:57 +1000

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
-import { ToDoService } from './to-do.service';
+import { AppComponent } from "./app.component";
+import { TodoComponent } from "./todo/todo.component";
+import { ToDoService } from "./to-do.service";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, TodoComponent ],
-  bootstrap:    [ AppComponent ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, HttpClientModule],
+  declarations: [AppComponent, TodoComponent],
+  bootstrap: [AppComponent],
   providers: [ToDoService]
 })
-export class AppModule { }
+export class AppModule {}
